f297c972bddebbc250a262ee3791175852f8d62f
Yuem Park <yuempark@berkeley.edu>
2019-10-11 11:33:32 -0400

    remove dropbox from sync scripts

--- bash-scripts/SyncUp.sh
+++ bash-scripts/SyncUp.sh
@@ -3,10 +3,7 @@
 
 open -a /Applications/Box\ Sync.app
 
-open -a /Applications/Dropbox.app
-
 open -a /Applications/Backup\ and\ Sync.app
 
 printf "   Opened: \033[0;35mBox Sync\033[0m\n"
-printf "   Opened: \033[0;35mDropbox\033[0m\n"
 printf "   Opened: \033[0;35mGoogle Drive\033[0m\n"
